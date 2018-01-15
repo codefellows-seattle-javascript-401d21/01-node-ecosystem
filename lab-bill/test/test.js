9cd4494e5b9b98d6b8114ae0fa4c48fa8a163605
billodell <billodell5@gmail.com>
2018-01-15 15:46:00 -0800

    deleted modules

--- lab-bill/test/test.js
+++ lab-bill/test/test.js
@@ -1,14 +1,4 @@
-// describe('Math Module', function() {
-//     describe('#Floor', function() {
-//       it('should take in a floating point number, and return the previous whole integer', function() {
-//         expect(math.floor(1.123)).toEqual(1)
-//         expect(math.floor(1.123)).not.toEqual(1.123)
-//       })
 
-//       it('should validate that the input for `num` is a numeric value', function() {
-//         // expect(...)
-//       })
-//     })
 describe('#Greet', function() {
 
   it('should return null if the input is not a string', function() {
