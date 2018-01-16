4511807c26c24f2f3a0ec9213ccc154bde264d91
Koko Kassa <kowserk7@gmail.com>
2018-01-15 18:04:09 -0800

    added aritmetic function

--- lab-koko/lib/arithmetic.js
+++ lab-koko/lib/arithmetic.js
@@ -2,4 +2,10 @@
 
 const math = module.exports = {};
 
-math.add = function
+math.add = function (num) {
+  if (typeof num !== 'number') {
+    return new Error ('null');
+  } else {
+    return a+b
+  }
+};
