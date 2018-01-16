d063b8f5dca5e55a99ee3b2f6709d9eb6bbef46f
Koko Kassa <kowserk7@gmail.com>
2018-01-15 17:40:40 -0800

    finished the arithmetic test

--- lab-koko/__test__/arithmetic.test.js
+++ lab-koko/__test__/arithmetic.test.js
@@ -4,8 +4,19 @@ const math = require('../lib/arithmetic.js');
 
 describe ('Math Module', function(){
   describe('#Add', function(){
-    it('should be a + b = c', function (){
+    it('should be a + b = c', function(){
       expect(math.add(3,5)).toEqual(8);
+    });
+    it ('should be a numerical value', function(){
+      expect(math.add('a','b')).toEqual('null');
+    });
+  });
+  describe('#Subtract', function(){
+    it ('should be a - b = c', function(){
+      expect(math.subtract(5,3)).toEqual(2);
+    });
+    it ('should be a numerical value', function(){
+      expect(math.subtract('a','b')).toEqual('null');
     });
   });
 });
