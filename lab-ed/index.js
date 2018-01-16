5a99f20997c8583ec813c4c4a6d774cbc53197c0
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-15 19:22:30 -0800

    add math tests

--- lab-ed/index.js
+++ lab-ed/index.js
@@ -3,6 +3,6 @@
 const greet = require('./lib/greet')
 const arithmetic = require('./lib/arithmetic')
 
-greet.hello('kai')
+greet.hello('world')
 arithmetic.add(5, 5)
 arithmetic.sub(6, 5)
--- lab-ed/test/arithmetic.test.js
+++ lab-ed/test/arithmetic.test.js
@@ -1,6 +1,26 @@
 'use strict'
 
-const greet = require('../lib/greet')
+const arithmetic = require('../lib/arithmetic')
 
-expect(greet.hello(5)).toBeNull
-expect(greet.hello('world')).toEqual('hello world')
+expect(arithmetic.add(5, 'str')).toBeNull
+expect(arithmetic.add(5, 5)).toEqual(10)
+expect(arithmetic.sub(5, 'str')).toBeNull
+expect(arithmetic.sub(6, 5)).toEqual(1)
+
+describe('Add Module', function() {
+  describe('#Add', function() {
+    it('should return the sum of two numbers', function() {
+      expect(arithmetic.add(5, 'str')).toBeNull
+      expect(arithmetic.add(5, 5)).toEqual(10)
+    })
+  })
+})
+
+describe('Sub Module', function() {
+  describe('#Sub', function() {
+    it('should return the result of subtracting the 2nd argument from the first', function() {
+      expect(arithmetic.sub(5, 'str')).toBeNull
+      expect(arithmetic.sub(6, 5)).toEqual(1)
+    })
+  })
+})
