ee95ea23403b5c1562d8b46c52fc6e769c0eb1e0
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-15 20:55:27 -0800

    finally done!

---
+++ lab-yohanes/__test__/greet.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const greet = require('../lib/greet.js');
+
+describe('Greet Module', function () {
+  describe('#Input', function () {
+    it('should take in a string', function () {
+      expect(greet(123)).toEqual(null);
+    });
+    it('should take in the argument world', function () {
+      expect(greet('World')).toEqual('Hello World');
+    });
+  });
+});
